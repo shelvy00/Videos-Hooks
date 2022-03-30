8623e4d1f3d8e4dc8a27cb60edfe414477324de8
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2022-03-30 18:38:21 -0500

    git init.

---
+++ src/components/VideoList.js
@@ -0,0 +1,18 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+export const VideoList = ({ videos, onVideoSelect }) => {
+    //props.videos
+   const renderedList = videos.map( video =>{
+      return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />
+    });
+
+
+  return(
+    <div className="ui relaxed divided list">
+      {renderedList}
+    </div>
+  );
+};
+
+export default VideoList;
